46a8bf3b1d0e75b1000154de5a4c46100060e157
Rodrigo Castagnaro <rodrigoskte@hotmail.com>
2024-05-16 15:09:25 -0300

    Subindo Domain

---
+++ DoctorAppointmentBooking.Domain/Entities/Patient.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DoctorAppointmentBooking.Domain.Entities
+{
+    public class Patient : BaseEntity
+    {
+        public string Name { get; private set; }
+        public string Email { get; private set; }
+        public bool IsDeleted { get; private set; }
+    }
+}
