7eb252e0a5191c9f27c6394064f21d266c8b21c3
Rodrigo Castagnaro <rodrigoskte@hotmail.com>
2024-05-16 15:08:28 -0300

    Subindo application

---
+++ DoctorAppointmentBooking.Application/Validators/PatientValidator.cs
@@ -0,0 +1,24 @@
+﻿using DoctorAppointmentBooking.Domain.Entities;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DoctorAppointmentBooking.Application.Validators
+{
+    public class PatientValidator : AbstractValidator<Patient>
+    {
+        public PatientValidator()
+        {
+            RuleFor(c => c.Name)
+                .NotEmpty().WithMessage("Please enter the name.")
+                .NotNull().WithMessage("Please enter the name.");
+
+            RuleFor(c => c.Email)
+                .NotEmpty().WithMessage("Please enter the email.")
+                .NotNull().WithMessage("Please enter the email.");
+        }
+    }
+}
