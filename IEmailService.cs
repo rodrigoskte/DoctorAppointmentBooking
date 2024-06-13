ee2f6a5ae428104683bc7c5d4c1c92329adc2974
Rodrigo Castagnaro <rodrigoskte@hotmail.com>
2024-06-13 12:21:23 -0300

    Delete IEmailService.cs

--- IEmailService.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace DoctorAppointmentBooking.Domain.Interfaces
-{
-    public interface IEmailService
-    {
-        Task SendEmail(string emailRecipient, string emailSubject, string emailBody);
-    }
-}
