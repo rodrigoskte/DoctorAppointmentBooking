27e2b6717a13acc35add48ca226a7e7399cb7249
Rodrigo Castagnaro <rodrigoskte@hotmail.com>
2024-06-13 12:18:39 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,240 @@
+using DoctorAppointmentBooking.Application.DTOs;
+using DoctorAppointmentBooking.Application.Services;
+using DoctorAppointmentBooking.Domain.Entities;
+using DoctorAppointmentBooking.Domain.Interfaces;
+using DoctorAppointmentBooking.Infrastructure.Context;
+using DoctorAppointmentBooking.Infrastructure.Repository;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using System.Text;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+ConfigureDbContext(builder);
+ConfigureIdentity(builder);
+ConfigureInjection(builder);
+ConfigureJwtSettings(builder);
+ConfigureSwagger(builder);
+
+var app = builder.Build();
+
+await ApplyMigrations(app);
+
+// Configure the HTTP request pipeline.
+app.UseSwagger();
+app.UseSwaggerUI();
+ConfigureCors(app);
+app.UseHttpsRedirection();
+app.UseAuthentication();
+app.UseAuthorization();
+app.MapControllers();
+await CreateDefaultRoles(app);
+await CreateDefaultAdminUser(app);
+
+app.Run();
+
+static void ConfigureInjection(WebApplicationBuilder webApplicationBuilder)
+{
+    webApplicationBuilder.Services.AddScoped<IBaseRepository<Patient>, BaseRepository<Patient>>();
+    webApplicationBuilder.Services.AddScoped<IBaseService<Patient>, BaseService<Patient>>();
+    webApplicationBuilder.Services.AddScoped<IBaseRepository<Specialty>, BaseRepository<Specialty>>();
+    webApplicationBuilder.Services.AddScoped<IBaseService<Specialty>, BaseService<Specialty>>();
+    webApplicationBuilder.Services.AddScoped<IBaseRepository<Doctor>, BaseRepository<Doctor>>();
+    webApplicationBuilder.Services.AddScoped<IBaseService<Doctor>, BaseService<Doctor>>();
+    webApplicationBuilder.Services.AddScoped<IBaseRepository<DoctorSpecialty>, BaseRepository<DoctorSpecialty>>();
+    webApplicationBuilder.Services.AddScoped<IBaseService<DoctorSpecialty>, BaseService<DoctorSpecialty>>();
+    webApplicationBuilder.Services.AddScoped<IBaseRepository<Schedule>, BaseRepository<Schedule>>();
+    webApplicationBuilder.Services.AddScoped<IBaseService<Schedule>, BaseService<Schedule>>();
+    webApplicationBuilder.Services.AddScoped<IDoctorRepository, DoctorRepository>();
+    webApplicationBuilder.Services.AddScoped<IDoctorService, DoctorService>();
+    webApplicationBuilder.Services.AddScoped<ISpecialtyRepository, SpecialtyRepository>();
+    webApplicationBuilder.Services.AddScoped<ISpecialtyService, SpecialtyService>();
+    webApplicationBuilder.Services.AddScoped<IDoctorSpecialtyRepository, DoctorSpecialtyRepository>();
+    webApplicationBuilder.Services.AddScoped<IDoctorSpecialtyService, DoctorSpecialtyService>();
+    webApplicationBuilder.Services.AddScoped<IScheduleRepository, ScheduleRepository>();
+    webApplicationBuilder.Services.AddScoped<IScheduleService, ScheduleService>();
+    webApplicationBuilder.Services.AddScoped<IPatientRepository, PatientRepository>();
+    webApplicationBuilder.Services.AddScoped<IPatientService, PatientService>();
+    webApplicationBuilder.Services.AddScoped<IPasswordHasher<IdentityUser>, PasswordHasher<IdentityUser>>();
+    webApplicationBuilder.Services.AddScoped<IAuthService, AuthService>();
+    webApplicationBuilder.Services.AddScoped<IUserManagerService, UserManagerService>();
+    webApplicationBuilder.Services.AddScoped<IEmailService, EmailService>();    
+}
+
+static void ConfigureDbContext(WebApplicationBuilder builder1)
+{
+    builder1.Services.AddDbContext<SqlDbContext>(options =>
+    {
+        options.UseSqlServer(builder1.Configuration.GetConnectionString("DefaultSqlConnection_dev"),
+            sqlServerOptionsAction: sqlOptions =>
+            {
+                sqlOptions.EnableRetryOnFailure(
+                    maxRetryCount: 5,
+                    maxRetryDelay: TimeSpan.FromSeconds(30),
+                    errorNumbersToAdd: null);
+            });
+    });
+
+    builder1.Services.AddDbContext<AuthDbContext>(options =>
+    {
+        options.UseSqlServer(builder1.Configuration.GetConnectionString("DefaultAuthSqlConnection"),
+            sqlServerOptionsAction: sqlOptions =>
+            {
+                sqlOptions.EnableRetryOnFailure(
+                    maxRetryCount: 5,
+                    maxRetryDelay: TimeSpan.FromSeconds(30),
+                    errorNumbersToAdd: null);
+            });
+    });
+
+    builder1.Services.AddControllersWithViews();
+}
+
+static async Task ApplyMigrations(WebApplication app)
+{
+    using (var scope = app.Services.CreateScope())
+    {
+        var services = scope.ServiceProvider;
+        var dbContext = services.GetRequiredService<SqlDbContext>();
+        await dbContext.Database.MigrateAsync();
+
+        var authDbContext = services.GetRequiredService<AuthDbContext>();
+        await authDbContext.Database.MigrateAsync();
+    }
+}
+
+static void ConfigureCors(WebApplication app)
+{
+    app.UseCors(builder =>
+    {
+        builder.AllowAnyMethod();
+        builder.AllowAnyHeader();
+        builder.AllowAnyOrigin();
+    });
+}
+
+static void ConfigureIdentity(WebApplicationBuilder builder)
+{
+    builder.Services.AddIdentity<IdentityUser, IdentityRole>(options =>
+        {
+            options.Password.RequireDigit = false;
+            options.Password.RequireLowercase = false;
+            options.Password.RequireNonAlphanumeric = false;
+            options.Password.RequireUppercase = false;
+            options.User.AllowedUserNameCharacters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-._@+";
+        })
+        .AddRoles<IdentityRole>()
+        .AddEntityFrameworkStores<AuthDbContext>()
+        .AddDefaultTokenProviders();
+}
+
+static void ConfigureJwtSettings(WebApplicationBuilder builder)
+{
+    var emailSettingsSection = builder.Configuration.GetSection("EmailSettings");
+    builder.Services.Configure<EmailSettings>(emailSettingsSection);
+
+    var jwtSettingsSection = builder.Configuration.GetSection("JwtSettings");
+    builder.Services.Configure<JwtSettings>(jwtSettingsSection);
+
+    var jwtSettings = jwtSettingsSection.Get<JwtSettings>();
+    var key = Encoding.ASCII.GetBytes(jwtSettings.Segredo);
+    builder.Services.AddAuthentication(options =>
+    {
+        options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+        options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+    }).AddJwtBearer(options =>
+    {
+        options.RequireHttpsMetadata = true;
+        options.SaveToken = true;
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            IssuerSigningKey = new SymmetricSecurityKey(key),
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidAudience = jwtSettings.Audiencia,
+            ValidIssuer = jwtSettings.Emissor
+        };
+    });
+}
+
+static void ConfigureSwagger(WebApplicationBuilder builder)
+{
+    builder.Services.AddSwaggerGen(c =>
+    {
+        c.SwaggerDoc("v1", new OpenApiInfo { Title = "DoctorAppointmentBooking", Version = "v1" });
+
+        c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme()
+        {
+            Name = "Authorization",
+            Type = SecuritySchemeType.ApiKey,
+            Scheme = "Bearer",
+            BearerFormat = "JWT",
+            In = ParameterLocation.Header,
+            Description =
+                "JWT Authorization header using the Bearer scheme. \r\n\r\n Enter 'Bearer' [space] and then your token in the text input below.\r\n\r\nExample: \"Bearer 1safsfsdfdfd\"",
+        });
+        c.AddSecurityRequirement(new OpenApiSecurityRequirement
+        {
+            {
+                new OpenApiSecurityScheme
+                {
+                    Reference = new OpenApiReference
+                    {
+                        Type = ReferenceType.SecurityScheme,
+                        Id = "Bearer"
+                    }
+                },
+                new string[] { }
+            }
+        });
+    });
+}
+
+static async Task CreateDefaultRoles(WebApplication app)
+{
+    using (var scope = app.Services.CreateScope())
+    {
+        var services = scope.ServiceProvider;
+        var roleManager = services.GetRequiredService<RoleManager<IdentityRole>>();
+        var roles = new[] { "Admin", "Patient", "Doctor" };
+        foreach (var role in roles)
+        {
+            if (!await roleManager.RoleExistsAsync(role))
+            {
+                await roleManager.CreateAsync(new IdentityRole(role));
+            }
+        }
+    }
+}
+
+static async Task CreateDefaultAdminUser(WebApplication app)
+{
+    using (var scope = app.Services.CreateScope())
+    {
+        var userManager = scope.ServiceProvider.GetRequiredService<UserManager<IdentityUser>>();
+        var adminUser = new IdentityUser
+        {
+            UserName = "admin@admin.com.br",
+            Email = "admin@admin.com.br",
+            EmailConfirmed = true
+        };
+
+        var user = await userManager.FindByEmailAsync(adminUser.Email);
+        if (user == null)
+        {
+            var result = await userManager.CreateAsync(
+                adminUser, 
+                "@senha01");
+            
+            if (result.Succeeded)
+            {
+                await userManager.AddToRoleAsync(adminUser, "Admin");
+            }
+        }
+    }
+}
---
+++ ScheduleController.cs
@@ -0,0 +1,220 @@
+﻿using DoctorAppointmentBooking.Application.Constants;
+using DoctorAppointmentBooking.Application.DTOs;
+using DoctorAppointmentBooking.Application.Validators;
+using DoctorAppointmentBooking.Domain.Entities;
+using DoctorAppointmentBooking.Domain.Interfaces;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DoctorAppointmentBooking.Presentation.API.Controllers;
+
+[Authorize]
+[ApiController]
+[Route("api/v1/[controller]/")]
+public class ScheduleController : BaseController
+{
+    private readonly IBaseService<Schedule> _baseScheduleService;
+    private readonly IScheduleService _scheduleService;
+    private readonly IPatientService _patientService;
+    private readonly IDoctorService _doctorService;
+    private readonly IEmailService _emailService;
+
+    public ScheduleController(
+        IBaseService<Schedule> baseScheduleService,
+        IScheduleService scheduleService,
+        IPatientService patientService,
+        IDoctorService doctorService,
+        IEmailService emailService)
+    {
+        _baseScheduleService = baseScheduleService;
+        _scheduleService = scheduleService;
+        _patientService = patientService;
+        _doctorService = doctorService;
+        _emailService = emailService;
+
+    }
+
+    [HttpGet]
+    public IActionResult Get()
+    {
+        return Execute(() => _scheduleService.GetAllWithDetails());
+    }
+
+    [HttpGet]
+    [Route("{id:int}")]
+    public IActionResult Get([FromRoute] int id)
+    {
+        if (id <= 0)
+            return NotFound();
+
+        return Execute(() => _scheduleService.GetAllWithDetailsId(id));
+    }
+
+    [HttpGet]
+    [Route("GetAllSchedules")]
+    public IActionResult GetAllSchedules()
+    {
+        return Execute(() => _baseScheduleService.Get());
+    }
+
+    [HttpPost]
+    public IActionResult Create([FromBody] ScheduleDto scheduleDto)
+    {
+        if (scheduleDto == null)
+            return NotFound();
+
+        return Execute(() =>
+        {
+            var schedule = new Schedule
+            {
+                DoctorId = scheduleDto.DoctorId,
+                PatientId = scheduleDto.PatientId,
+                DateTimeSchedule = scheduleDto.DateTimeSchedule,
+                IsDeleted = scheduleDto.IsDeleted
+            };
+
+            _scheduleService.Validations(schedule);
+
+            _baseScheduleService.Add<ScheduleValidator>(schedule);
+
+            _emailService.SendEmail(
+                ((schedule.Doctor.Email) + "," + schedule.Patient.Email),
+                EmailConstants.SUBJECT_SCHEDULE_SUCESS,
+                EmailConstants.SUBJECT_SCHEDULE_SUCESS_BODY);
+
+            return scheduleDto;
+        });
+    }
+
+    [HttpPut]
+    [Route("{id:int}")]
+    public IActionResult Update([FromRoute] int id,
+        [FromBody] ScheduleDto scheduleDto)
+    {
+        if (id <= 0 || scheduleDto == null)
+            return NotFound();
+
+        return Execute(() =>
+        {
+            var schedule = new Schedule
+            {
+                Id = id,
+                DoctorId = scheduleDto.DoctorId,
+                PatientId = scheduleDto.PatientId,
+                DateTimeSchedule = scheduleDto.DateTimeSchedule,
+                IsDeleted = scheduleDto.IsDeleted
+            };
+
+            _scheduleService.Validations(schedule);
+            _baseScheduleService.Update<ScheduleValidator>(schedule);
+
+            _emailService.SendEmail(
+                ((schedule.Doctor.Email) + "," + schedule.Patient.Email),
+                EmailConstants.SUBJECT_SCHEDULE_CHANGED,
+                EmailConstants.SUBJECT_SCHEDULE_CHANGED_BODY);
+
+            return scheduleDto;
+        });
+    }
+
+    [HttpDelete]
+    [Route("{id:int}")]
+    public IActionResult Delete([FromRoute] int id)
+    {
+        if (id <= 0)
+            return NotFound();
+
+        return Execute(() =>
+        {
+            var schedule = _baseScheduleService.GetById(id);
+            _baseScheduleService.Delete(id);
+
+            _emailService.SendEmail(
+                ((schedule.Doctor.Email) + "," + schedule.Patient.Email),
+                EmailConstants.SUBJECT_SCHEDULE_CANCELED,
+                EmailConstants.SUBJECT_SCHEDULE_CANCELED_BODY);
+
+            return true;
+        });
+    }
+
+    [HttpGet]
+    [Route("GetPatientSchedule/{patientId}")]
+    public IActionResult GetPatientSchedule(
+        [FromRoute] string patientId)
+    {
+        if (string.IsNullOrEmpty(patientId))
+            return NotFound();
+
+        var patient = _patientService.GetPatientByUserId(patientId);
+        if (patient == null)
+            return NotFound();
+
+        return Execute(() => _scheduleService.GetAllSchedulePatientId(patient.Id));
+    }
+
+    [HttpGet]
+    [Route("GetDoctorSchedule/{doctorId}")]
+    public IActionResult GetDoctorSchedule(
+        [FromRoute] string doctorId)
+    {
+        if (string.IsNullOrEmpty(doctorId))
+            return NotFound();
+
+        var doctor = _doctorService.GetDoctorByUserId(doctorId);
+        if (doctor == null)
+            return NotFound();
+
+        return Execute(() => _scheduleService.GetAllScheduleDoctorId(doctor.Id));
+    }
+
+    [HttpPut]
+    [Route("CancelSchedulePatient/{patientId}")]
+    public IActionResult CancelSchedulePatient(
+        [FromRoute] string patientId)
+    {
+        if (string.IsNullOrEmpty(patientId))
+            return NotFound();
+
+        var patient = _patientService.GetPatientByUserId(patientId);
+        if (patient == null)
+            return NotFound();
+
+        return Execute(() =>
+        {
+            var schedule = new Schedule
+            {
+                PatientId = patient.Id,
+                IsDeleted = true
+            };
+
+            _scheduleService.CancelSchedulePatient(patient.Id);
+            return patientId;
+        });
+    }
+
+    [HttpPut]
+    [Route("CancelScheduleDoctor/{doctorId}")]
+    public IActionResult CancelScheduleDoctor(
+        [FromRoute] string doctorId)
+    {
+        if (string.IsNullOrEmpty(doctorId))
+            return NotFound();
+
+        var doctor = _doctorService.GetDoctorByUserId(doctorId);
+        if (doctor == null)
+            return NotFound();
+
+        return Execute(() =>
+        {
+            var schedule = new Schedule
+            {
+                DoctorId = doctor.Id,
+                IsDeleted = true
+            };
+
+            _scheduleService.CancelScheduleDoctor(doctor.Id);
+            return doctorId;
+        });
+    }
+}
